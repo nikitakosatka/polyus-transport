9305e312d7e738f49836ef8d55f4539109899ca2
nikitakosatka <nikitakosatka@yandex.ru>
2022-10-15 10:01:01 +0300

    transport_type CRUD

---
+++ backend/pkg/api/service/transport_type.py
@@ -0,0 +1,29 @@
+from backend.pkg.api.repository import transport_type
+
+
+def create(application, db):
+    transport_type.create(application, db)
+
+
+def get_all(db):
+    return transport_type.get_all(db)
+
+
+def get(id, db):
+    return transport_type.get(id, db)
+
+
+def update(id, request, db):
+    return transport_type.update(id, request, db)
+
+
+def remove(id, db):
+    return transport_type.remove(id, db)
+
+
+def is_available_transport(transport_type_id, date_start, date_end, db):
+    if transport_type.get_available_transport(transport_type_id, date_start,
+                                              date_end, db):
+        return True
+
+    return False
