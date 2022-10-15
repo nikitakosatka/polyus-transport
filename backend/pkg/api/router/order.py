bd21ee7326ba4b69eff0feaf6447b8209c5899c4
nikitakosatka <nikitakosatka@yandex.ru>
2022-10-15 10:00:42 +0300

    order CRUD

---
+++ backend/pkg/api/router/order.py
@@ -0,0 +1,42 @@
+from uuid import UUID
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from backend.pkg import schema
+from backend.pkg.db import get_db
+from backend.pkg.api.service import order
+
+router = APIRouter(prefix='/api/order', tags=['Order'])
+
+
+@router.post('/create')
+async def create(request: schema.Order, db: Session = Depends(get_db)):
+    order.create(request, db)
+
+
+@router.get('/all')
+async def get_all(db: Session = Depends(get_db)):
+    return order.get_all(db)
+
+
+@router.get('/{id}')
+async def get(id: UUID, db: Session = Depends(get_db)):
+    return order.get(id, db)
+
+
+@router.put('/{id}')
+async def update(id: UUID, request: schema.Order,
+                 db: Session = Depends(get_db)):
+    return order.update(id, request, db)
+
+
+@router.delete('/{id}')
+async def remove(id: UUID, db: Session = Depends(get_db)):
+    return order.remove(id, db)
+
+
+@router.get('/by_status')
+async def get_by_status(status: schema.OrderStatus,
+                        db: Session = Depends(get_db)):
+    return order.get_by_status(status, db)
