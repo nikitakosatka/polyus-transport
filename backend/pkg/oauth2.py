42d1775466c777d3fd6622ad9bc205caccc02086
nikitakosatka <nikitakosatka@yandex.ru>
2022-10-15 14:57:17 +0300

    fix

--- backend/pkg/oauth2.py
+++ backend/pkg/oauth2.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
-from blog import token
+from backend.pkg import token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
